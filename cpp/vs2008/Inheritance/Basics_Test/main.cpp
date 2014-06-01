ac006db5ea6a5d12c2b25fb8e3399dd909460570
Andriy Bun <andr.bun@gmail.com>
2014-06-01 23:13:04 +0200

    Set up the solution

--- cpp/vs2008/Inheritance/Basics_Test/main.cpp
+++ cpp/vs2008/Inheritance/Basics_Test/main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, sample_test)
-{
-	EXPECT_EQ(1, 1);
-}
+#include "Polygon.h"
+
+//class rectangleTest : public ::testing::Test 
+//{
+//protected:
+//	virtual void SetUp() 
+//	{
+//		
+//	}
+//
+//	rectangle<float> rect;
+//};
+//
+//TEST_F(rectangleTest, TestRectangleArea)
+//{
+//	int width = 3;
+//	int height = 4;
+//	rectangle<float> rect; // (width, height)
+//	EXPECT_EQ(rect.get_area(), width * height);
+//}
 
 int main(int argc, char **argv)
 {
-	::testing::InitGoogleTest(&argc, argv);
+	int width = 3;
+	int height = 4;
+	rectangle<float> rect(width, height);
+	//EXPECT_EQ(rect.get_area(), width * height);
+	//EXPECT_EQ(rect.get_area(), 2);
+
+	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
+//int main(int argc, char **argv)
+//{
+//	rectangle<float> rect(4, 5);
+//	std::cout << "The area of rectangle is: " << rect.get_area() << std::endl;
+//	std::cout << "My name is " << rect.get_name() << std::endl;
+//	rect.print_properties();
+//	return 0;
+//}
