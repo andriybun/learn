58d85bb2762bc6647ebd185c6bb23f0c566e1193
Andriy Bun <andr.bun@gmail.com>
2014-08-20 17:19:38 +0200

    Testing virtual inheritance

--- cpp/vs2008/Inheritance/Virtual/main.cpp
+++ cpp/vs2008/Inheritance/Virtual/main.cpp
@@ -2,18 +2,29 @@
 
 #include "vehicle.h"
 
-TEST(vehiclesTest, testInheritance)
-{
-	vehicle* bmw = new car;
-	bmw->print();
-	vehicle* bombardier = new boat;
-	bombardier->print();
-	vehicle* luaz = new amphibian;
-	luaz->print();
-}
+// TEST(vehiclesTest, testInheritance)
+// {
+// 	vehicle* bmw = new car;
+// 	bmw->print();
+// 	vehicle* bombardier = new boat;
+// 	bombardier->print();
+// 	vehicle* luaz = new amphibian;
+// 	luaz->print();
+// }
 
 int main(int argc, char **argv)
 {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    // testing::InitGoogleTest(&argc, argv);
+    // return RUN_ALL_TESTS();
+    vehicle* bmw = new car;
+    bmw->print();
+
+    // vehicle* bombardier = new boat;
+    // bombardier->print();
+    // delete bombardier;
+    vehicle* luaz = new amphibian;
+    luaz->print();
+    delete luaz;
+    delete bmw;
+    return 0;
 }
